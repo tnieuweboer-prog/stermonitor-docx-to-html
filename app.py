bb35dedc2c39d9e56339e66a38f9ebe9c8adf6d4
tnieuweboer-prog <t.nieuweboer@atlascollege.nl>
2025-11-05 17:29:27 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+import streamlit as st
+from html_converter import docx_to_html
+from pptx_converter import docx_to_pptx
+# plus eventuele Cloudinary-setup hier of in html_converter via callback
+
+st.title("Triade DOCX Converter")
+tab1, tab2 = st.tabs(["HTML", "PowerPoint"])
+
+with tab1:
+    uploaded = st.file_uploader("Upload voor HTML", type=["docx"])
+    if uploaded:
+        html = docx_to_html(uploaded, image_uploader=None, platform="Stermonitor")
+        st.code(html, language="html")
+
+with tab2:
+    uploaded2 = st.file_uploader("Upload voor PPTX", type=["docx"])
+    if uploaded2:
+        pptx_bytes = docx_to_pptx(uploaded2)
+        st.download_button("Download PPTX", data=pptx_bytes, file_name="lessonup.pptx")
